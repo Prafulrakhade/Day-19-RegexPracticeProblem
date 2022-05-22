cc75f9168f1186d61815d8b54fac531bfd9cea53
Praful <prafulrakhade02@gmail.com>
2022-05-22 14:37:07 +0530

    [praful] Added UC_4_Pre-Defined Mobile Format Regex Program File

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace RegexProblems
 {
@@ -7,6 +8,23 @@ namespace RegexProblems
         static void Main(string[] args)
         {
             Console.WriteLine("\n\n----------------WELCOME TO THE REGULAR EXPRESSION (REGEX) PROGRAM------------------------\n\n");
+            while(true)
+            {
+                Console.Write("Enter User Mobile Number with Contry Code: ");
+                string mobile = Console.ReadLine();
+                Console.WriteLine();
+                string mo = "^[9][1][ ]+[6-9][0-9]{9}$";
+                if (Regex.IsMatch(mobile, mo))
+                {
+                    Console.WriteLine("User Mobile Number is : " + mobile);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please Enter Valid mobile Number with Country Code : " + "\n\n");
+                }
+
+            }  
         }
     }
 }
