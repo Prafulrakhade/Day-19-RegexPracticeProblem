b08802269a58381c313c8e05048d8685a7238bef
Praful <prafulrakhade02@gmail.com>
2022-05-22 15:54:11 +0530

    [praful] Added UC_6_password format with minumum 8 character and at least one upper case Regex Program File

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace RegexProblems
 {
@@ -7,6 +8,23 @@ namespace RegexProblems
         static void Main(string[] args)
         {
             Console.WriteLine("\n\n----------------WELCOME TO THE REGULAR EXPRESSION (REGEX) PROGRAM------------------------\n\n");
+            while(true)
+            {
+                Console.WriteLine("Enter User Password, should be Minimum 8 char,at least one UpperCase: ");
+                string password = Console.ReadLine();
+                Console.WriteLine();
+                string pass = "[A-Za-z]+[A-Za-z]{7,}";
+                if (Regex.IsMatch(password, pass))
+                {
+                    Console.WriteLine("User Password is : " + password);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please Enter Valid password should be Minimum 8 char, at least one UpperCase: " + "\n\n");
+                }
+
+            }  
         }
     }
 }
