7a940a938351ee3171c8352697d9f2bd28a44f22
Praful <prafulrakhade02@gmail.com>
2022-05-22 14:51:13 +0530

    [praful] Added UC_5_password format with minumum 8 character Regex Program File

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace RegexProblems
 {
@@ -7,6 +8,23 @@ namespace RegexProblems
         static void Main(string[] args)
         {
             Console.WriteLine("\n\n----------------WELCOME TO THE REGULAR EXPRESSION (REGEX) PROGRAM------------------------\n\n");
+            while(true)
+            {
+                Console.Write("Enter User Password, should be Minimum 8 char: ");
+                string password = Console.ReadLine();
+                Console.WriteLine();
+                string pass = "^[a-zA-Z 0-9]{8,}$";
+                if (Regex.IsMatch(password, pass))
+                {
+                    Console.WriteLine("User Password is : " + password);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please Enter Valid password should be Minimum 8 char: " + "\n\n");
+                }
+
+            }  
         }
     }
 }
