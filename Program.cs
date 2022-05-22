3991a819fc9c3aade159860201c03b51942a9b66
Praful <prafulrakhade02@gmail.com>
2022-05-22 23:27:05 +0530

    [praful] Added : program file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace RegexProblems
 {
@@ -7,6 +8,22 @@ namespace RegexProblems
         static void Main(string[] args)
         {
             Console.WriteLine("\n\n----------------WELCOME TO THE REGULAR EXPRESSION (REGEX) PROGRAM------------------------\n\n");
+            while (true)
+            {
+                Console.Write("Enter User Email ID : ");
+                string name = Console.ReadLine();
+                Console.WriteLine();
+                string nm = "^[A-Za-z0-9.+-]{1,}[a-zA-Z0-9]@[a-z0-9]{1,}.(co|org|com|in|us|net|au|com.com|com.au)$"; 
+                if (Regex.IsMatch(name, nm))
+                {
+                    Console.WriteLine("User Email ID is : " + name);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please Enter Valid Email ID  : " + "\n\n");
+                }
+            }
         }
     }
 }
