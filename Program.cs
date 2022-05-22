2f8cef1ccb9550c31c94a1a4ceb6f038da79c359
Praful <prafulrakhade02@gmail.com>
2022-05-22 14:21:37 +0530

    [praful] Added UC_3_Email ID Regex Program File

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace RegexProblems
 {
@@ -7,6 +8,23 @@ namespace RegexProblems
         static void Main(string[] args)
         {
             Console.WriteLine("\n\n----------------WELCOME TO THE REGULAR EXPRESSION (REGEX) PROGRAM------------------------\n\n");
+            while(true)
+            {
+                Console.Write("Enter User Email ID : ");
+                string name = Console.ReadLine();
+                Console.WriteLine();
+                string nm = "^[a-z]+.[a-z]+@?[a-z]+.(co|com|org)|.(in|us|co|com)$";  //(Ex. abc.xyz@bl.co.in) last .in is optional
+                if (Regex.IsMatch(name, nm))
+                {
+                    Console.WriteLine("User Email ID is : " + name);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please Enter Valid Email ID : \n\n Ex. abc.xyz@bl.co.in : " + "\n\n");
+                }
+
+            }  
         }
     }
 }
